0309200c8e9a2faf2d9fed9c7ab2fa7dc1f77068
Carvalho <carvalho.pedro.cardoso@gmail.com>
2019-07-16 08:56:37 -0300

    Tha-Tha-Tha-Tha-That's All Folks

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const nunjunks = require('nunjucks');
-const app = express();
+const express = require('express')
+const nunjunks = require('nunjucks')
+const app = express()
 nunjunks.configure('views', {
-    autoescape: true,
-    express: app,
-    watch: true,
-});
-app.use(express.urlencoded({extended:false,}));
-const logMiddleware = (req, res, next) => {
-    console.log(`HOST: ${req.headers.host} | URL: ${req.url}  | METHOD: ${req.method}`);
-    return next();
-}
-app.set("view engine", "njk");
-app.use(logMiddleware);
-const  users = ["Pedro","cardoso","carvalho",] 
-app.get('/', (req, res) => {
-    res.render("list",{users});
-});
-app.get('/new',(req,res)=>{
-    res.render('new');
+  autoescape: true,
+  express: app,
+  watch: true
 })
-app.post("/create",(req,res)=>{
-    users.push(req.body.user);
-    res.redirect('/');
+app.use(express.urlencoded({ extended: false }))
+const logMiddleware = (req, res, next) => {
+  console.log(`HOST: ${req.headers.host} | URL: ${req.url}  | METHOD: ${req.method}`)
+  return next()
+}
+app.set('view engine', 'njk')
+app.use(logMiddleware)
+const users = ['Pedro', 'cardoso', 'carvalho']
+app.get('/', (req, res) => {
+  res.render('list', { users })
+})
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  res.redirect('/')
 })
 
-app.listen(3000);
+app.listen(3000)
